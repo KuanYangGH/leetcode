9894ab94e2c9ddd577fbc8f073e170578c0769d4
KuanYangGH <ykxjtuedu@163.com>
2018-10-08 15:48:44 +0800

    question768:Max Chunks To Make Sorted II

--- src/hard/question768/Solution.java
+++ src/hard/question768/Solution.java
@@ -13,6 +13,33 @@ package hard.question768;
  */
 public class Solution {
     public int maxChunksToSorted(int[] arr) {
-        return 0;
+        if(arr==null||arr.length==0){
+            return 0;
+        }
+        int length = arr.length;
+        int[] maxOfLeft = new int[length];
+        int[] minOfRight = new int[length];
+
+        maxOfLeft[0] = arr[0];
+        for(int i=1;i<length;i++){
+            maxOfLeft[i] = maxOfLeft[i-1]>arr[i]?maxOfLeft[i-1]:arr[i];
+        }
+
+        minOfRight[length-1]=arr[length-1];
+        for(int i=length-2;i>=0;i--){
+            minOfRight[i] = minOfRight[i+1]<arr[i]?minOfRight[i+1]:arr[i];
+        }
+        int cnt = 1;
+        for(int i=0;i<length-1;i++){
+            if(maxOfLeft[i]<=minOfRight[i+1]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        System.out.println(new Solution().maxChunksToSorted(arr));
     }
 }
