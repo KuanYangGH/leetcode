cd7b483131c01ee0463a8b329b2530d7d80a2a47
KuanYangGH <ykxjtuedu@163.com>
2018-11-09 16:40:10 +0800

    question14:Longest Common Prefix

---
+++ src/easy/question14/Solution.java
@@ -0,0 +1,33 @@
+package easy.question14;
+
+
+/**
+ *  Longest Common Prefix
+ *  Write a function to find the longest common prefix string amongst an array of strings.
+ *  If there is no common prefix, return an empty string "".
+ * @author yangkuan
+ * @date 2018/11/09
+ */
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0){
+            return "";
+        }
+        else if(strs.length==1){
+            return strs[0];
+        }
+        for(int i=0;i<strs[0].length();i++){
+            for(int j=1;j<strs.length;j++){
+                if(strs[j].length()<i+1||strs[0].charAt(i)!=strs[j].charAt(i)){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flow","flow","flod"};
+        System.out.print(new Solution().longestCommonPrefix(strs));
+    }
+}
